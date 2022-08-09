ccb167afa3a0208bf53f695beff30149aec23689
RST595 <rst5995@gmail.com>
2022-08-09 19:45:09 +0300

    Fix StudentRequest.java

--- src/main/java/com/aston/dto/StudentRequest.java
+++ src/main/java/com/aston/dto/StudentRequest.java
@@ -2,9 +2,11 @@ package com.aston.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class StudentRequest {
     private String firstName;
     private String lastName;
