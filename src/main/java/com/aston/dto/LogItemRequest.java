9c6652eaed7b591f70fca2b8423e07f56ab0ce30
RST595 <rst5995@gmail.com>
2022-08-09 19:47:02 +0300

    Fix LogItemRequest.java

--- src/main/java/com/aston/dto/LogItemRequest.java
+++ src/main/java/com/aston/dto/LogItemRequest.java
@@ -2,10 +2,12 @@ package com.aston.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class LogItemRequest {
     private String message;
     private String date;
